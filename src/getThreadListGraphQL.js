21af5736e48ef3431afc82aa68c8550e473de237
ravkr <rav_kr@interia.pl>
2018-01-13 16:26:17 +0100

    getThreadListGraphQL: few more things

--- src/getThreadListGraphQL.js
+++ src/getThreadListGraphQL.js
@@ -3,11 +3,23 @@
 var utils = require("../utils");
 var log = require("npmlog");
 
-function formatListGraphQLResponse(data) {
+function formatThreadList(data) {
   return data.map((t) => {
     return {
       threadID: t.thread_key.thread_fbid || t.thread_key.other_user_id,
       name: t.name,
+      // lastMessage: t.last_message, // TODO
+      unreadCount: t.unread_count,
+      messageCount: t.messages_count,
+      imageSrc: t.image?t.image.uri:null,
+      emoji: t.customization_info?t.customization_info.emoji:null,
+      muteUntil: data.mute_until,
+      // : t.,
+      // : t.,
+      nicknames: t.customization_info?t.customization_info.participant_customizations.map((u) => { return {"userID": u.participant_id, "nickname": u.nickname }; }):[],
+      // adminIDs: t.thread_admins, // feature from future? it is always an empty array - for more than a year
+      // customizationEnabled: t.customization_enabled; // TODO: always true? (was true even when customization_info was null)
+      threadType: data.thread_type, // "GROUP" or "ONE_TO_ONE"
     }
   })
 }
@@ -41,7 +53,7 @@ module.exports = function(defaultFuncs, api, ctx) {
           throw resData;
         }
         // callback(null, resData[0].o0.data.viewer.message_threads.nodes)
-        callback(null, formatListGraphQLResponse(resData[0].o0.data.viewer.message_threads.nodes));
+        callback(null, formatThreadList(resData[0].o0.data.viewer.message_threads.nodes));
         // This returns us an array of things. The last one is the success /
         // failure one.
         // @TODO What do we do in this case?

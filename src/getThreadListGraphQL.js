6afe91798289ca9535bb3503edff8292883c48f5
ravkr <rav_kr@interia.pl>
2018-01-13 19:36:12 +0100

    getThreadListGraphQL: participants

--- src/getThreadListGraphQL.js
+++ src/getThreadListGraphQL.js
@@ -3,6 +3,29 @@
 var utils = require("../utils");
 var log = require("npmlog");
 
+function formatParticipants(participants) {
+  return participants.nodes.map((p)=>{
+    p = p.messaging_actor;
+    console.log(p.messaging_actor);
+    return {
+      userID: p.id,
+      name: p.name,
+      shortName: p.short_name,
+      gender: p.gender, // MALE, FEMALE
+      url: p.url, // how about making it profileURL
+      profilePicture: p.big_image_src.uri,
+      username: p.username,
+      // TODO: maybe better names for these?
+      isViewerFriend: p.is_viewer_friend, // true/false
+      isMessengerUser: p.is_messenger_user, // true/false
+      isVerified: p.is_verified, // true/false
+      isMessageBlockedByViewer: p.is_message_blocked_by_viewer, // true/false
+      isViewerCoworker: p.is_viewer_coworker, // true/false
+      isEmployee: p.is_employee // null? when it is something other? can someone check?
+    };
+  });
+}
+
 function formatThreadList(data) {
   return data.map((t) => {
     return {
@@ -14,14 +37,52 @@ function formatThreadList(data) {
       imageSrc: t.image?t.image.uri:null,
       emoji: t.customization_info?t.customization_info.emoji:null,
       muteUntil: data.mute_until,
+      participants: formatParticipants(t.all_participants),
       // : t.,
       // : t.,
       nicknames: t.customization_info?t.customization_info.participant_customizations.map((u) => { return {"userID": u.participant_id, "nickname": u.nickname }; }):[],
+      color: t.customization_info?t.customization_info.outgoing_bubble_color:null, // TODO: what to return? "FF8C0077" or null
       // adminIDs: t.thread_admins, // feature from future? it is always an empty array - for more than a year
       // customizationEnabled: t.customization_enabled; // TODO: always true? (was true even when customization_info was null)
       threadType: data.thread_type, // "GROUP" or "ONE_TO_ONE"
     }
   })
+}
+
+function formatThread(data) {
+  return {
+    // threadID: formatID(data.thread_fbid.toString()),
+    // participants: data.participants.map(formatID),
+    participantIDs: data.participants.map(formatID),
+    // name: data.name,
+    // nicknames: data.custom_nickname,
+    snippet: data.snippet,
+    snippetAttachments: data.snippet_attachments,
+    snippetSender: formatID((data.snippet_sender || '').toString()),
+    // unreadCount: data.unread_count,
+    // messageCount: data.message_count,
+    // imageSrc: data.image_src,
+    timestamp: data.timestamp,
+    serverTimestamp: data.server_timestamp, // what is this?
+    // muteUntil: data.mute_until,
+    isCanonicalUser: data.is_canonical_user,
+    isCanonical: data.is_canonical,
+    isSubscribed: data.is_subscribed,
+    folder: data.folder,
+    isArchived: data.is_archived,
+    recipientsLoadable: data.recipients_loadable,
+    hasEmailParticipant: data.has_email_participant,
+    readOnly: data.read_only,
+    canReply: data.can_reply,
+    cannotReplyReason: data.cannot_reply_reason,
+    lastMessageTimestamp: data.last_message_timestamp,
+    lastReadTimestamp: data.last_read_timestamp,
+    lastMessageType: data.last_message_type,
+    // emoji: data.custom_like_icon,
+    color: data.custom_color,
+    // adminIDs: data.admin_ids,
+    // threadType: data.thread_type
+  };
 }
 
 module.exports = function(defaultFuncs, api, ctx) {
